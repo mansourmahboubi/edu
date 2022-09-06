acefbc27e0102090004ac48a9bfea9dbe87211b5
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-09-06 07:22:10 +0200

    feat: add threading

---
+++ threading/thread.py
@@ -0,0 +1,31 @@
+from concurrent.futures import ThreadPoolExecutor
+from time import perf_counter, sleep
+from threading import Thread,active_count, current_thread
+import sys
+
+DAEMON_BOOL = False
+start = perf_counter()
+
+def show(name, sleep_seconds=3):
+    print(f'{name} started')
+    sleep(sleep_seconds)
+    print(current_thread())
+    print(f'{name} finished')
+# thread module
+# t1 = Thread(target=show, args=('t1',5),name='shoobool', daemon=DAEMON_BOOL)
+# t2 = Thread(target=show, args=('t2',),name='bala', daemon=DAEMON_BOOL)
+
+# t1.start()
+# t2.start()
+
+# pool executer
+
+with ThreadPoolExecutor(max_workers=2) as executor:
+    names = ['t1','t2','t3']
+    executor.map(show, names)
+
+end = perf_counter()
+
+print(f'Elapsed time: {end - start}')
+print(f'Threads alive: {active_count()}')
+sys.exit()
