20cb0b8011be3c7e70b6a43820e37b681575c7fc
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-09-07 10:59:20 +0200

    feat: custom factory method

---
+++ design-pattern/patterns/creational/2_factory_method/04_factory_method.py
@@ -0,0 +1,64 @@
+"""
+    products
+    books -> Edu, History, Biology
+"""
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class BaseCreator(ABC):
+    @abstractmethod
+    def factory_method(self):
+        pass
+
+    def process(self):
+        product = self.factory_method()
+        excerpt = product.excerpt()  # type: ignore
+        return excerpt
+
+
+class HistoryBookCreator(BaseCreator):
+    def factory_method(self) -> "BaseBook":
+        return HistoryBook()
+
+
+class EduBookCreator(BaseCreator):
+    def factory_method(self) -> "BaseBook":
+        return EduBook()
+
+
+class BaseBook(ABC):
+    """
+    Concrete products
+    """
+
+    @abstractmethod
+    def excerpt(self) -> "List[str]":
+        pass
+
+
+class HistoryBook(BaseBook):
+    def excerpt(self):
+        return ["Boosh", "Merkel", "Ajmadinejad"]
+
+
+class EduBook(BaseBook):
+    def excerpt(self):
+        return ["Learn", "How", "Why"]
+
+
+class BiologyBook(BaseBook):
+    def excerpt(self):
+        return ["Human", "Body", "Blood"]
+
+
+def client(creator: "BaseCreator"):
+    print(creator.process())
+
+
+if __name__ == "__main__":
+    creator = HistoryBookCreator()
+    client(creator)
+
+    creator_2 = EduBookCreator()
+    client(creator_2)
