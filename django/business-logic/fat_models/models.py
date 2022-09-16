dd35a685b58ccc09ec5829e76dbde9f4e68fbb28
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-09-16 21:35:17 +0200

    feat: add fat models example

---
+++ django/business-logic/fat_models/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+
+
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    street = models.CharField(max_length=50)
+    number = models.PositiveIntegerField()
+    is_default = models.BooleanField()
+
+    @property
+    def full_address(self):
+        return f"{self.street}, {self.number}"
+
+    # Business logic inside the model
+    def set_default(self):
+        # Dependencies with other models (in this case the same one)
+        Address.objects.filter(user=self.user).update(is_default=False)
+        self.is_default = True
+        self.save()
+
+        # Side-effects of the action inside the model, SRP ko
+        # events.publish(events.DEFAULT_ADDRESS_CHANGED, address=self)
+
+    def __str__(self):
+        return f"{self.user}-{self.street}"
---
+++ django/business-logic/fat_models/tests.py
@@ -0,0 +1,41 @@
+import logging
+
+import pytest
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from .models import Address
+
+logger = logging.getLogger(__name__)
+
+User = get_user_model()
+
+
+class TestSetDefault:
+    @pytest.fixture
+    def user(self):
+        return User(
+            first_name="test",
+            last_name="test last name",
+            email="asdf@email.com",
+            username="asdf@email.com",
+        )
+
+    @pytest.fixture
+    def address(self, user):
+        return Address(user=user, street="Fake", number=1, is_default=False)
+
+    def test_sets_address_as_default_and_publishes_changes(self, mocker, address):
+        mock_filter = mocker.patch.object(Address.objects, "filter")
+        mock_update = mocker.patch.object(mock_filter.return_value, "update")
+        mock_save = mocker.patch.object(address, "save")
+        # mock_publish = mocker.patch.object(events, 'publish')
+
+        address.set_default()
+        logger.error(address)
+        # We lose the integration tests of the database flows
+        mock_filter.assert_called_with(user=address.user)
+        mock_update.assert_called_with(is_default=False)
+        assert address.is_default == True
+        mock_save.assert_called()
+        # mock_publish.assert_called_with(events.DEFAULT_ADDRESS_CHANGED, address=address)
