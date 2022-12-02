b885e98be642f2de687283db2f782171aa2fa949
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-12-02 21:00:35 +0100

    feat: udpate generator example

--- async/example/04-asyncio/generator/simple_gen.py
+++ async/example/04-asyncio/generator/simple_gen.py
@@ -1,28 +1,38 @@
-from typing import List
+from typing import Generator, List
+
+from mansour_utils.timer import Timer
 
 
-# def fib(n: int) -> List[int]:
-#     numbers = []
-#     current, nxt = 0, 1
-#     while len(numbers) < n:
-#         current, nxt = nxt, current + nxt
-#         numbers.append(current)
-#
-#     return numbers
+def fib(n: int) -> List[int]:
+    numbers = []
+    current, nxt = 0, 1
+    while len(numbers) < n:
+        current, nxt = nxt, current + nxt
+        numbers.append(current)
 
-def fib():
+    return numbers
+
+
+def fib_gen() -> Generator[int, None, None]:
     current, nxt = 0, 1
     while True:
         current, nxt = nxt, current + nxt
         yield current
 
 
-result = fib()
+with Timer("Method 1"):
+    result = fib(21)
+    print(result)
 
-for n in result:
-    print(n, end=', ')
-    if n > 10000:
-        break
+with Timer("Method gen"):
+    result = fib_gen()
+    print(result)
+
+    for n in result:
+        print(n, end=", ")
+        if n > 10000:
+            break
+    print("")
 
 print()
 print("Done")
