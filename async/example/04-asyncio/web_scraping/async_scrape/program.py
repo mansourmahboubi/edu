96dcb4de07c3c422e18d2829130b9606d5cae1a2
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-12-03 16:25:54 +0100

    chore: asynio scrapping

--- async/example/04-asyncio/web_scraping/async_scrape/program.py
+++ async/example/04-asyncio/web_scraping/async_scrape/program.py
@@ -13,7 +13,7 @@ global loop
 async def get_html(episode_number: int) -> str:
     print(Fore.YELLOW + f"Getting HTML for episode {episode_number}", flush=True)
 
-    url = f'https://talkpython.fm/{episode_number}'
+    url = f"https://talkpython.fm/{episode_number}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
@@ -24,8 +24,8 @@ async def get_html(episode_number: int) -> str:
 
 def get_title(html: str, episode_number: int) -> str:
     print(Fore.CYAN + f"Getting TITLE for episode {episode_number}", flush=True)
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-    header = soup.select_one('h1')
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    header = soup.select_one("h1")
     if not header:
         return "MISSING"
 
@@ -66,5 +66,5 @@ async def get_title_range():
         print(Fore.WHITE + f"Title found: {title}", flush=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
