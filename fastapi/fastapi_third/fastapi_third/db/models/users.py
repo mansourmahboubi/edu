68c77148fe3a026c8da5106c9a12392cfe34edac
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2022-12-04 20:59:26 +0100

    feat: add fast api third example

---
+++ fastapi/fastapi_third/fastapi_third/db/models/users.py
@@ -0,0 +1,18 @@
+# from typing import TYPE_CHECKING
+
+from sqlalchemy import Boolean, Column, Integer, String
+
+from fastapi_third.db.base import Base
+
+# from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
+    # items = relationship("Item", back_populates="owner")
