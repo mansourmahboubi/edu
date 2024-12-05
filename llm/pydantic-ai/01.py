26172694d2add02a2d44ce378d07eedc52d3ac9d
Mansour Mahboubi <mansourmahboubi@protonmail.com>
2024-12-05 15:02:10 +0100

    feat: pydantic init

---
+++ llm/pydantic-ai/01.py
@@ -0,0 +1,14 @@
+from pydantic_ai import Agent
+
+agent = Agent(
+    "openai:gpt-4o",
+    system_prompt="Be concise, reply with one sentence.",
+)
+
+result = agent.run_sync('Where does "hello world" come from?')
+
+
+print(result.data)
+"""
+The first known use of "hello, world" was in a 1974 textbook about the C programming language.
+"""
